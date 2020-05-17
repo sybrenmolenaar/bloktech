a81dd1ab96901594813812a3f9c042940a28b3f0
Sybren Molenaar <sybren.molenaar@hva.nl>
2020-05-17 17:17:18 +0200

    Add express and routing

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+// Statically serve public folder
+app.use(express.static('public'))
+
+// Set up routes
+const routes = {
+  '/': 'index.html',
+  '/about': 'about.html',
+  '/contact': 'contact.html',
+  '*': '404.html',
+}
+
+for (let [route, source] of Object.entries(routes)) {
+  app.get(route, (req, res) => res.sendFile(`${__dirname}/public/${source}`))
+}
+
+app.listen(port, () => {  
+  console.log(`App running on http://localhost:${port}`)
+})
