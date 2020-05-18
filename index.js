bd686c763d8aba180eed6cef1d53831f5da68a79
Sybren Molenaar <sybren.molenaar@hva.nl>
2020-05-18 12:07:59 +0200

    Changes

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const handlebars = require('express-handlebars')
 
 // Statically serve public folder
 app.use(express.static('public'))
+
+app.engine('hbs', handlebars({ extname: 'hbs' }))
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+// app.get('/users', (req, res) => {
+//   res.render('users.hbs')
+//  })
 
 
 const users = {
@@ -20,37 +29,49 @@ const users = {
     ]
   }
 }
-// Set up routes
-// app.get('/users/:userId', (req, res) => { 
-//   if (req.params.userId > users.length - 1) { 
-//     res.send(`User ${req.params.userId} not found`)
-//   } else {
-//     res.send(`Hello ${users[req.params.userId].name}`)
-//   }
-// })
-// http://localhost:3000/users/0
 
-
-app.get('/users/:username/photos/:photoId', (req, res) => { 
-  // console.log(req.params)
-  // res.send(`This is the profile of @${req.params.username}`)
-  const user = req.params.username // 'sybrenmolenaar'
-  const photoId = req.params.photoId // 1
-  console.log(photoId)
-  res.send(`
-    <h1>Hi ${users[user].name}, jouw username is @${user}</h1>
-    
-    Dit is jouw foto:
-    
-    <img src="${users[user].photos[photoId]}"/>`)
+// http://localhost:3000/users/didiercatz/
+app.get('/users/:username', (req, res) => { 
+  
+  const username = req.params.username
+  const photoId = req.params.photoId
+  
+  res.render('users.hbs', {
+    username: username,
+    name: users[username].name,
+  })
 })
 
+
 // http://localhost:3000/users/didiercatz/photos/0
-// http://localhost:3000/users/sybrenmolenaar/photos/0
+app.get('/users/:username/photos/:photoId', (req, res) => { 
+  
+  const username = req.params.username
+  const photoId = req.params.photoId
+  
+  res.render('users.hbs', {
+    username: username,
+    name: users[username].name,
+    photo: users[username].photos[photoId]
+  })
+})
+app.get('/about', (req,res) => {
+    res.render('about.hbs', {
+        title:'About Me',
+        description:'Hallo, ik ben Sybren'
+    });
+});
+
+app.get('/home', (req,res) => {
+    res.render('home.hbs', {
+    title:'Home page',
+    description:'Home of MusicLovers'
+    })
+})
 
 const routes = {
   '/': 'index.html',
-  '/about': 'about.html',
+  '/about': 'views/about.hbs',
   '/contact': 'contact.html',
   '*': '404.html',
 }
